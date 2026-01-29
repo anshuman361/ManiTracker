7506d91525a15b2bbf908a56b8c1220f76580d23
anshuman361 <pandeyanshuman361@gmail.com>
2026-01-29 18:43:27 +0530

    added mongodb atlas

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,9 @@ const transactionRouter = require("./routes/transactionRouter");
 const app = express();
 
 mongoose
-  .connect("mongodb://127.0.0.1:27017/expenseTracker")
+  .connect(
+    "mongodb+srv://pandeyanshuman361_db_user:NNcvKumDfzHwVqMH@manitracker.akewotg.mongodb.net/?appName=ManiTracker",
+  )
   .then(() => {
     console.log("MongoDB connected");
   })
